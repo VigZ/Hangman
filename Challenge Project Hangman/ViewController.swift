766c70ff6d0293ca221b83a70b0565f1b63be582
vigz <kyle.vigorito@gmail.com>
2020-07-24 12:39:42 -0400

    Initial Commit

---
+++ Challenge Project Hangman/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Challenge Project Hangman
+//
+//  Created by Kyle on 7/24/20.
+//  Copyright © 2020 Kyle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
